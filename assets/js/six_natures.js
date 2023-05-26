d48d17addd876ff1931d7fd785b4d0ebb61291fe
paddler2022 <Luyuheng0310@outlook.com>
2023-05-26 12:18:32 +0200

    change into hsitogram

--- assets/js/six_natures.js
+++ assets/js/six_natures.js
@@ -25,121 +25,41 @@ d3.csv("pokemon-data-updated.csv").then(function(data) {
       return +d.Speed;
     });
   
-    // 定义分布范围
-    var ranges_hp = {
-      "0-60": 0,
-      "60-90": 0,
-      "90-110": 0,
-      "110+": 0
+    var hpData = {
+      x: hpValues,
+      type: 'histogram',
+      name: 'HP'
     };
-  
-    var ranges_attack = {
-      "0-60": 0,
-      "60-90": 0,
-      "90-110": 0,
-      "110+": 0
+
+    var attackData = {
+      x: attackValues,
+      type: 'histogram',
+      name: 'Attack'
     };
-  
-    var ranges_defense = {
-      "0-60": 0,
-      "60-90": 0,
-      "90-110": 0,
-      "110+": 0
+
+    var defenseData = {
+      x: defenseValues,
+      type: 'histogram',
+      name: 'Defense'
     };
-  
-    var ranges_spattack = {
-      "0-60": 0,
-      "60-90": 0,
-      "90-110": 0,
-      "110+": 0
+
+    var specialAttackData = {
+      x: specialAttackValues,
+      type: 'histogram',
+      name: 'Special Attack'
     };
-  
-    var ranges_spdefense = {
-      "0-60": 0,
-      "60-90": 0,
-      "90-110": 0,
-      "110+": 0
+
+    var specialDefenseData = {
+      x: specialDefenseValues,
+      type: 'histogram',
+      name: 'Special Defense'
     };
-  
-    var ranges_speed = {
-      "0-60": 0,
-      "60-90": 0,
-      "90-110": 0,
-      "110+": 0
+
+    var speedData = {
+      x: speedValues,
+      type: 'histogram',
+      name: 'Speed'
     };
-  
-    // 统计各范围内的数量
-    hpValues.forEach(function(value) {
-      if (value <= 60) {
-        ranges_hp["0-60"]++;
-      } else if (value <= 90) {
-        ranges_hp["60-90"]++;
-      } else if (value <= 110) {
-        ranges_hp["90-110"]++;
-      } else {
-        ranges_hp["110+"]++;
-      }
-    });
-  
-    attackValues.forEach(function(value) {
-      if (value <= 60) {
-        ranges_attack["0-60"]++;
-      } else if (value <= 90) {
-        ranges_attack["60-90"]++;
-      } else if (value <= 110) {
-        ranges_attack["90-110"]++;
-      } else {
-        ranges_attack["110+"]++;
-      }
-    });
-  
-    defenseValues.forEach(function(value) {
-      if (value <= 60) {
-        ranges_defense["0-60"]++;
-      } else if (value <= 90) {
-        ranges_defense["60-90"]++;
-      } else if (value <= 110) {
-        ranges_defense["90-110"]++;
-      } else {
-        ranges_defense["110+"]++;
-      }
-    });
-  
-    specialAttackValues.forEach(function(value) {
-      if (value <= 60) {
-        ranges_spattack["0-60"]++;
-      } else if (value <= 90) {
-        ranges_spattack["60-90"]++;
-      } else if (value <= 110) {
-        ranges_spattack["90-110"]++;
-      } else {
-        ranges_spattack["110+"]++;
-      }
-    });
-  
-    specialDefenseValues.forEach(function(value) {
-      if (value <= 60) {
-        ranges_spdefense["0-60"]++;
-      } else if (value <= 90) {
-        ranges_spdefense["60-90"]++;
-      } else if (value <= 110) {
-        ranges_spdefense["90-110"]++;
-      } else {
-        ranges_spdefense["110+"]++;
-      }
-    });
-  
-    speedValues.forEach(function(value) {
-      if (value <= 60) {
-        ranges_speed["0-60"]++;
-      } else if (value <= 90) {
-        ranges_speed["60-90"]++;
-      } else if (value <= 110) {
-        ranges_speed["90-110"]++;
-      } else {
-        ranges_speed["110+"]++;
-      }
-    });
   
     // 将分布转换为绘图数据
     var plot_HP = Object.keys(ranges_hp).map(function(key) {
@@ -166,86 +86,19 @@ d3.csv("pokemon-data-updated.csv").then(function(data) {
       return { label: key, value: ranges_speed[key] };
     });
   
-    // 创建饼图
-    var data_HP = [{
-      values: plot_HP.map(function(d) { return d.value; }),
-      labels: plot_HP.map(function(d) { return d.label; }),
-      type: "bar"
-    }];
-  
-    var data_Attack = [{
-      values: plot_Attack.map(function(d) { return d.value; }),
-      labels: plot_Attack.map(function(d) { return d.label; }),
-      type: "bar"
-    }];
-  
-    var data_Special_Attack = [{
-      values: plot_Special_Attack.map(function(d) { return d.value; }),
-      labels: plot_Special_Attack.map(function(d) { return d.label; }),
-      type: "bar"
-    }];
-  
-    var data_Defense = [{
-      values: plot_Defense.map(function(d) { return d.value; }),
-      labels: plot_Defense.map(function(d) { return d.label; }),
-      type: "bar"
-    }];
-  
-    var data_Special_Defense = [{
-      values: plot_Special_Defense.map(function(d) { return d.value; }),
-      labels: plot_Special_Defense.map(function(d) { return d.label; }),
-      type: "bar"
-    }];
-  
-    var data_Speed = [{
-      values: plot_Speed.map(function(d) { return d.value; }),
-      labels: plot_Speed.map(function(d) { return d.label; }),
-      type: "bar"
-    }];
-  
-    // 定义布局
-    var layout_HP = {
-      title: "HP Stats Distribution",
-      height: 400,
-      width: 500
+    var layout = {
+      barmode: 'overlay',
+      xaxis: { title: 'Stat Value' },
+      yaxis: { title: 'Count' },
+      title: 'Pokemon Data Histogram'
     };
-  
-    var layout_Attack = {
-      title: "Attack Stats Distribution",
-      height: 400,
-      width: 500
-    };
-  
-    var layout_Special_Attack = {
-      title: "Special Attack Stats Distribution",
-      height: 400,
-      width: 500
-    };
-  
-    var layout_Defense = {
-      title: "Defense Stats Distribution",
-      height: 400,
-      width: 500
-    };
-  
-    var layout_Special_Defense = {
-      title: "Special Defense Stats Distribution",
-      height: 400,
-      width: 500
-    };
-  
-    var layout_Speed = {
-      title: "Speed Stats Distribution",
-      height: 400,
-      width: 500
-    };
-  
-    // 绘制图表
-    Plotly.newPlot("HP_chart", data_HP, layout_HP);
-    Plotly.newPlot("Attack_chart", data_Attack, layout_Attack);
-    Plotly.newPlot("Special_Attack_chart", data_Special_Attack, layout_Special_Attack);
-    Plotly.newPlot("Defense_chart", data_Defense, layout_Defense);
-    Plotly.newPlot("Special_Defense_chart", data_Special_Defense, layout_Special_Defense);
-    Plotly.newPlot("Speed_chart", data_Speed, layout_Speed);
+
+    // 将数据和布局配置绘制成图表
+    Plotly.newPlot('HP_chart', [hpData], layout);
+    Plotly.newPlot('Attack_chart', [attackData], layout);
+    Plotly.newPlot('Defense_chart', [defenseData], layout);
+    Plotly.newPlot('Special_Attack_chart', [specialAttackData], layout);
+    Plotly.newPlot('Special_Defense_chart', [specialDefenseData], layout);
+    Plotly.newPlot('Speed_chart', [speedData], layout);
   });
   
